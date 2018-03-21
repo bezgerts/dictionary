c5240145934a8d3056eab53ac818cf00ed2b4282
Аркадий Безгерц <bezgerts@gmail.com>
2018-03-21 10:00:28 +0300

    Убрал ненужные зависимости в pom.xml. Добавил дефолтный контроллер и представление для теста.

---
+++ src/main/java/me/bezgerts/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package me.bezgerts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "/home";
+    }
+}
