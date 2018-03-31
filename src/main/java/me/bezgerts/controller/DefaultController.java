6d4dc33bc0c4fd913a1778f618a5dc54500216ff
Аркадий Безгерц <bezgerts@gmail.com>
2018-03-31 12:43:02 +0300

    Добавил таблицу и левое меню.

--- src/main/java/me/bezgerts/controller/DefaultController.java
+++ src/main/java/me/bezgerts/controller/DefaultController.java
@@ -2,7 +2,6 @@ package me.bezgerts.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
