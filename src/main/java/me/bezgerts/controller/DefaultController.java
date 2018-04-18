84e23bbd8d270cebf8570af5701d93261078bc24
Bezgerts Arkdaiy <bezgerts@gmail.com>
2018-04-18 20:09:59 +0300

    Исправил баг со слешем в маршруте /login

--- src/main/java/me/bezgerts/controller/DefaultController.java
+++ src/main/java/me/bezgerts/controller/DefaultController.java
@@ -24,11 +24,11 @@ public class DefaultController {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
     }
 
     @GetMapping("/403")
     public String error403() {
-        return "/error/403";
+        return "error/403";
     }
 }
